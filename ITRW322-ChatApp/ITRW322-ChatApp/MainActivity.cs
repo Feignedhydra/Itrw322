dc6181718fa20428828ad45cfcc6395a0163ca46
Unknown <25961926@student.g.nwu.ac.za>
2017-08-21 07:20:22 +0200

    Created Login layout for Companion App
    Basic layout with login button and signup button

---
+++ ITRW322-ChatApp/ITRW322-ChatApp/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.OS;
+using Android.Linq;
+
+namespace ITRW322_ChatApp
+{
+    [Activity(Label = "ITRW322_ChatApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            //Create button
+            Button button = FindViewById<btn_Signup>(Resources.Id.btn_Signup);
+
+            button.Click =  SetContentView(Resource.Layout.Sign_up);
+
+        }
+    }
+}
+
