bd111299939e8809bbbf0f7da25c539c85a305ae
Unknown <25961926@student.g.nwu.ac.za>
2017-09-13 12:33:14 +0200

    Created message service

--- ChatApp/app/src/main/java/com/example/student/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/example/student/chatapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import com.firebase.ui.auth.AuthUI;
@@ -15,6 +17,7 @@ import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
 
         activity_main = (RelativeLayout)findViewById(R.id.activity_main);
         fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void OnClick(View view)
+            {
+                EditText input = (EditText)findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(input.getText().toString(),FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+                input.setText("");
+            }
+        });
 
         //Check if not sign-in then navigate Signing page
         if(FirebaseAuth.getInstance().getCurrentUser() == null)
